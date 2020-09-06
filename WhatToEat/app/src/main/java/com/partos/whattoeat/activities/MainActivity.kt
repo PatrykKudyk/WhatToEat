9c4baac4532795e4a31493e90798d7fe6218e3f2
Papryk <gustaw5022@gmail.com>
2020-09-06 15:37:27 +0200

    added menu

--- WhatToEat/app/src/main/java/com/partos/whattoeat/activities/MainActivity.kt
+++ WhatToEat/app/src/main/java/com/partos/whattoeat/activities/MainActivity.kt
@@ -3,10 +3,24 @@ package com.partos.whattoeat.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.partos.whattoeat.R
+import com.partos.whattoeat.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = MainFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_bottom_to_top,
+                R.anim.exit_top_to_bottom,
+                R.anim.enter_top_to_bottom,
+                R.anim.exit_bottom_to_top
+            )
+            .add(R.id.main_frame_layout, fragment)
+            .commit()
     }
 }
