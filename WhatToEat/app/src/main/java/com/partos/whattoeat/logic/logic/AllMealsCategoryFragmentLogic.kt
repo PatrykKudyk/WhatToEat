5c21ab8f73193598d527883cd243224578e473c5
Papryk <gustaw5022@gmail.com>
2020-09-07 17:37:18 +0200

    added showup

--- WhatToEat/app/src/main/java/com/partos/whattoeat/logic/logic/AllMealsCategoryFragmentLogic.kt
+++ WhatToEat/app/src/main/java/com/partos/whattoeat/logic/logic/AllMealsCategoryFragmentLogic.kt
@@ -7,29 +7,29 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.partos.whattoeat.R
 import com.partos.whattoeat.adapters.MarginItemDecoration
-import com.partos.whattoeat.adapters.recycler.MealTypeRecyclerViewAdapter
+import com.partos.whattoeat.adapters.recycler.MealsRecyclerViewAdapter
 import com.partos.whattoeat.db.DataBaseHelper
-import com.partos.whattoeat.logic.listeners.MealCategoriesFragmentListeners
+import com.partos.whattoeat.logic.listeners.AllMealsCategoryFragmentListeners
 
-class MealCategoriesFragmentLogic {
+class AllMealsCategoryFragmentLogic {
 
     private lateinit var recyclerView: RecyclerView
 
-    fun initFragment(rootView: View, fragmentManager: FragmentManager) {
+    fun initFragment(rootView: View, fragmentManager: FragmentManager, typeId: Long) {
         attachViews(rootView)
-        attachRecyclerView(rootView.context)
-        MealCategoriesFragmentListeners().initListeners(rootView, fragmentManager)
+        attachRecyclerView(rootView.context, typeId)
+        AllMealsCategoryFragmentListeners().initListeners(rootView, fragmentManager)
     }
 
-    private fun attachRecyclerView(context: Context) {
+    private fun attachRecyclerView(context: Context, typeId: Long) {
         val mLayoutManager = LinearLayoutManager(context)
         recyclerView.layoutManager = mLayoutManager
         recyclerView.addItemDecoration(MarginItemDecoration(12))
         val db = DataBaseHelper(context)
-        recyclerView.adapter = MealTypeRecyclerViewAdapter(db.getMealTypeList())
+        recyclerView.adapter = MealsRecyclerViewAdapter(db.getMealList(typeId))
     }
 
     private fun attachViews(rootView: View) {
-        recyclerView = rootView.findViewById(R.id.meal_categories_recycler_view)
+        recyclerView = rootView.findViewById(R.id.all_meals_category_recycler_view)
     }
 }
