c6b8186ca3d6f9c73955718ac5f350a20c02d612
Papryk <gustaw5022@gmail.com>
2020-09-14 19:33:27 +0200

    added delete option

--- WhatToEat/app/src/main/java/com/partos/whattoeat/adapters/recycler/MealsRecyclerViewAdapter.kt
+++ WhatToEat/app/src/main/java/com/partos/whattoeat/adapters/recycler/MealsRecyclerViewAdapter.kt
@@ -6,15 +6,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.partos.whattoeat.R
 import com.partos.whattoeat.activities.MainActivity
+import com.partos.whattoeat.db.DataBaseHelper
 import com.partos.whattoeat.fragments.meal.MealShowFragment
 import com.partos.whattoeat.models.Meal
-import kotlinx.android.synthetic.main.row_meal.view.*
+import kotlinx.android.synthetic.main.row_meal_extended.view.*
 
 class MealsRecyclerViewAdapter(var mealsList: ArrayList<Meal>) :
     RecyclerView.Adapter<MealsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MealsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val cell = inflater.inflate(R.layout.row_meal, parent, false)
+        val cell = inflater.inflate(R.layout.row_meal_extended, parent, false)
         return MealsViewHolder(cell)
     }
 
@@ -23,8 +24,10 @@ class MealsRecyclerViewAdapter(var mealsList: ArrayList<Meal>) :
     }
 
     override fun onBindViewHolder(holder: MealsViewHolder, position: Int) {
-        holder.view.row_meal_name.text = mealsList[position].name
-        holder.view.row_meal_card.setOnClickListener {
+        holder.view.row_meal_extended_constraint_normal.visibility = View.VISIBLE
+        holder.view.row_meal_extended_constraint_delete.visibility = View.GONE
+        holder.view.row_meal_extended_name.text = mealsList[position].name
+        holder.view.row_meal_extended_card.setOnClickListener {
             val fragment = MealShowFragment.newInstance(mealsList[position].id)
             (holder.view.context as MainActivity).supportFragmentManager
                 .beginTransaction()
@@ -35,6 +38,27 @@ class MealsRecyclerViewAdapter(var mealsList: ArrayList<Meal>) :
                 .replace(R.id.main_frame_layout, fragment)
                 .addToBackStack(MealShowFragment.toString())
                 .commit()
+        }
+        holder.view.row_meal_extended_delete.setOnClickListener {
+            holder.view.row_meal_extended_constraint_normal.visibility = View.GONE
+            holder.view.row_meal_extended_constraint_delete.visibility = View.VISIBLE
+        }
+        holder.view.row_meal_extended_yes.setOnClickListener {
+            holder.view.row_meal_extended_constraint_normal.visibility = View.VISIBLE
+            holder.view.row_meal_extended_constraint_delete.visibility = View.GONE
+            val db = DataBaseHelper(holder.view.context)
+            val ingredients = db.getIngredientList(mealsList[position].id)
+            for (ingredient in ingredients) {
+                db.deleteIngredient(ingredient.id)
+            }
+            db.deleteMeal(mealsList[position].id)
+            mealsList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, mealsList.size)
+        }
+        holder.view.row_meal_extended_no.setOnClickListener {
+            holder.view.row_meal_extended_constraint_normal.visibility = View.VISIBLE
+            holder.view.row_meal_extended_constraint_delete.visibility = View.GONE
         }
     }
 
