c8fb1b7c64b5779caa424bdb5a20fdc821b769fc
Papryk <gustaw5022@gmail.com>
2020-09-15 21:29:16 +0200

    fixes done

--- WhatToEat/app/src/main/java/com/partos/whattoeat/logic/generation/logic/GenerateChooseMealFragmentLogic.kt
+++ WhatToEat/app/src/main/java/com/partos/whattoeat/logic/generation/logic/GenerateChooseMealFragmentLogic.kt
@@ -2,6 +2,7 @@ package com.partos.whattoeat.logic.generation.logic
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.partos.whattoeat.R
@@ -12,10 +13,27 @@ import com.partos.whattoeat.db.DataBaseHelper
 class GenerateChooseMealFragmentLogic {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var noText1: TextView
+    private lateinit var noText2: TextView
 
     fun initFragment(rootView: View, mealTypeId: Long) {
         attachViews(rootView)
-        attachRecyclerView(rootView.context, mealTypeId)
+        checkDatabase(rootView.context, mealTypeId)
+    }
+
+    private fun checkDatabase(context: Context, mealTypeId: Long) {
+        val db = DataBaseHelper(context)
+        val meals = db.getMealList(mealTypeId)
+        if (meals.size == 0) {
+            noText1.visibility = View.VISIBLE
+            noText2.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        } else {
+            noText1.visibility = View.GONE
+            noText2.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            attachRecyclerView(context, mealTypeId)
+        }
     }
 
     private fun attachRecyclerView(context: Context, mealTypeId: Long) {
@@ -29,5 +47,7 @@ class GenerateChooseMealFragmentLogic {
 
     private fun attachViews(rootView: View) {
         recyclerView = rootView.findViewById(R.id.generate_choose_meal_recycler_view)
+        noText1 = rootView.findViewById(R.id.generate_choose_meal_none_1)
+        noText2 = rootView.findViewById(R.id.generate_choose_meal_none_2)
     }
 }
--- WhatToEat/app/src/main/java/com/partos/whattoeat/logic/generation/logic/GenerateChooseMealTypeFragmentLogic.kt
+++ WhatToEat/app/src/main/java/com/partos/whattoeat/logic/generation/logic/GenerateChooseMealTypeFragmentLogic.kt
@@ -2,6 +2,7 @@ package com.partos.whattoeat.logic.generation.logic
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.partos.whattoeat.R
@@ -12,10 +13,27 @@ import com.partos.whattoeat.db.DataBaseHelper
 class GenerateChooseMealTypeFragmentLogic {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var noText1: TextView
+    private lateinit var noText2: TextView
 
     fun initFragment(rootView: View) {
         attachViews(rootView)
-        attachRecyclerView(rootView.context)
+        checkDatabase(rootView.context)
+    }
+
+    private fun checkDatabase(context: Context) {
+        val db = DataBaseHelper(context)
+        val mealTypes = db.getMealTypeList()
+        if (mealTypes.size == 0) {
+            noText1.visibility = View.VISIBLE
+            noText2.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        } else {
+            noText1.visibility = View.GONE
+            noText2.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            attachRecyclerView(context)
+        }
     }
 
     private fun attachRecyclerView(context: Context) {
@@ -29,5 +47,7 @@ class GenerateChooseMealTypeFragmentLogic {
 
     private fun attachViews(rootView: View) {
         recyclerView = rootView.findViewById(R.id.generate_choose_meal_type_recycler_view)
+        noText1 = rootView.findViewById(R.id.generate_choose_meal_type_none_1)
+        noText2 = rootView.findViewById(R.id.generate_choose_meal_type_none_2)
     }
 }
